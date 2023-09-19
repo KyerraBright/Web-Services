1df6aed7e9fbb19060608651446443a2820232d0
KyerraBright <Kyerra@comcast.net>
2023-09-19 14:49:06 -0600

    working

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
+console.log("past first const!");
+
 const port = process.env.PORT || 8080;
 const app = express();
+console.log("past second const!");
 
 app
   .use(bodyParser.json())
@@ -13,12 +15,15 @@ app
     next();
   })
   .use('/', require('./routes'));
+console.log("past the app function!");
 
 mongodb.initDb((err, mongodb) => {
+  console.log("Inside the initDb callback!"); // Check if this message is logged
   if (err) {
-    console.log(err);
+    console.log("Error:", err); // Log the error message
   } else {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
+  console.log("Done!");
 });
--- db/connect.js
+++ db/connect.js
@@ -2,24 +2,32 @@ const dotenv = require('dotenv');
 dotenv.config();
 console.log(process.env.MONGODB_URI)
 const MongoClient = require('mongodb').MongoClient;
+console.log('in the connect.js');
 
 
 let _db;
-
+console.log('in the connect.js');
 const initDb = (callback) => {
-  if (_db) {
-    console.log('Db is already initialized!');
-    return callback(null, _db);
-  }
-  MongoClient.connect(process.env.MONGODB_URI)
-    .then((client) => {
-      _db = client;
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
-    });
-};
+    if (_db) {
+      console.log('Db is already initialized!');
+      return callback(null, _db);
+    }
+    
+    console.log('Initializing database...'); // Add this line to log the initialization
+    
+    MongoClient.connect(process.env.MONGODB_URI)
+      .then((client) => {
+        console.log('second function');
+        _db = client;
+        callback(null, _db);
+      })
+      .catch((err) => {
+        console.log('error');
+        callback(err);
+      });
+    console.log('Done with connect.js');
+  };
+  
 
 const getDb = () => {
   if (!_db) {
