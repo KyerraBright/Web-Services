ce302cd778719ac9dac44d3c9eeeb443e14c8242
KyerraBright <Kyerra@comcast.net>
2023-09-26 11:30:54 -0600

    working

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
-const { MongoClient } = require("mongodb");
+const express = require("express");
+const app = express();
+const port = 3000;
+const contactsRoutes = require("./express"); // Import your routes
 
-async function run() {
-  // TODO: Replace the placeholder connection string below with your Atlas cluster specifics.
-  const uri ="mongodb+srv://kyerra:March12001@cluster0.6vwto1x.mongodb.net/";
+app.use(express.json());
 
-  const client = new MongoClient(uri);
+app.use("/contacts", contactsRoutes); // Mount your contacts routes at /contacts
 
-  try {
-    await client.connect();
-
-    const dbName = "Contacts";
-    const collectionName = "Contacts";
-
-    const database = client.db(dbName);
-    const collection = database.collection(collectionName);
-
-    const findAllQuery = {}; // An empty query matches all documents
-
-    const findAllResult = await collection.find(findAllQuery).toArray();
-
-    if (findAllResult.length === 0) {
-      console.log("No documents found in the 'Contacts' collection.\n");
-    } else {
-      console.log(`Found ${findAllResult.length} documents in the 'Contacts' collection:\n${JSON.stringify(findAllResult)}\n`);
-    }
-  } catch (err) {
-    console.error(`Something went wrong: ${err}\n`);
-  } finally {
-    // Close the MongoDB connection when done.
-    await client.close();
-  }
-}
-
-// Call the run function to start the process.
-run();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
