177eb52807cfc695753026e78e6738fae14146c9
KyerraBright <Kyerra@comcast.net>
2023-09-26 12:06:26 -0600

    working?

--- app.js
+++ app.js
@@ -10,3 +10,27 @@ app.use("/contacts", contactsRoutes); // Mount your contacts routes at /contacts
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the container where you want to display the JSON content
+  const jsonContainer = document.getElementById("json-container");
+
+  // Fetch the JSON data from the file
+  fetch("contact.json")
+      .then((response) => response.json())
+      .then((data) => {
+          // Convert the JSON data to a string with proper formatting
+          const jsonString = JSON.stringify(data, null, 2);
+
+          // Create a <pre> element to preserve formatting
+          const preElement = document.createElement("pre");
+          preElement.textContent = jsonString;
+
+          // Append the <pre> element to the container
+          jsonContainer.appendChild(preElement);
+      })
+      .catch((error) => {
+          console.error("Error loading JSON:", error);
+      });
+});
+
