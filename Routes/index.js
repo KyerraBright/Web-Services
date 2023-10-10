8a0926e7b843a9c778424e24f59d4f8c40d04333
KyerraBright <Kyerra@comcast.net>
2023-10-10 10:45:08 -0600

    changing

--- Routes/index.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-
-const controllers = require("../Controllers"); // Use the correct path
-//check spellings
-
-routes.get('/', controllers.Books);
-
-module.exports = routes;
