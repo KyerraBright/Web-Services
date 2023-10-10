f08dc9fcf351d8108fa4494ec36d5c6c342c32a0
KyerraBright <Kyerra@comcast.net>
2023-10-10 09:41:38 -0600

    Starting Personal Project

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-
-const controllers = require("../controller"); // Use the correct path
-
-
-routes.get('/', controllers.PersonOne);
-
-module.exports = routes;
